04bc21edbd77cd3de8dc0144e5083f4ec3ae417f
Ian Johnson <ijohnson@wolfram.com>
2016-04-10 21:40:42 -0500

    Initial commit of the script to add users

---
+++ add_users.py
@@ -0,0 +1,44 @@
+
+#to run command line programs
+from os import system
+
+#the name of the file to read in
+userListFileName = "users.txt"
+
+group = "student"
+
+if group == "student":
+	semester = "spring16"
+else:
+	semester = ""
+
+
+#open up the file
+userListFile = open(userListFileName,'r')
+
+#put all the lines in as users
+userNames = []
+for line in userListFile:
+	trimmedLine = line.strip()
+	#ignore commented out lines
+	if len(trimmedLine)!=0 and trimmedLine[0] != '#':
+		#if we're adding student's we want to prepend s to the tech ID numbers
+		if group == "student":
+			userNames.append("s"+trimmedLine)
+		else:
+			userNames.append(trimmedLine)
+
+#now that we have the list of users, make all their home directories
+print("creating semester directory")
+system("mkdir /export/home/"+group+"/"+semester)
+system("chgrp student /export/home/"+group+"/"+semester)
+
+#now create the user
+print("creating users")
+for user in userNames:
+	system("sudo useradd -d /export/home/"+group+"/"+semester+"/"+user+" -m "+user)
+	
+#finally set their passwords
+print("setting passwords")
+for user in userNames:
+	system("sudo passwd "+user)
